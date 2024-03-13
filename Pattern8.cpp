382d2855f5efbc8b23ca1c2457f8052d158f5440
Harsh Upreti <harsh.upreti123@gmail.com>
2024-03-13 18:13:47 +0530

    Pattern 8

---
+++ Pattern8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    int row = 0;
+    while(row < n) {
+        int col = 0;
+        int count = row+1;
+        while(col <= row) {
+            cout<<count++<<" ";
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+    
+    return 0;
+}
